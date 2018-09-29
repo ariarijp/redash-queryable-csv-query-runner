a3dad95598704c46647ee8190868a3f689c01f49
ariarijp <takuya.arita@gmail.com>
2018-09-29 11:41:05 +0900

    Refactoring

--- redash_queryable_csv_query_runner/queryable_csv.py
+++ redash_queryable_csv_query_runner/queryable_csv.py
@@ -61,13 +61,7 @@ class QueryableCsv(BaseSQLQueryRunner):
 
         return columns, rows
 
-    def run_query(self, query, user):
-        query = query.strip()
-        path = str(self.configuration.get('path'))
-        delimiter = str(self.configuration.get('delimiter', ','))
-        if delimiter == 'TAB':
-            delimiter = "\t"
-
+    def _load_csv_to_table(self, conn, path, delimiter):
         columns, rows = self._read_csv(path, delimiter)
 
         create_table = u'CREATE TABLE csv ({columns});'.format(
@@ -76,33 +70,47 @@ class QueryableCsv(BaseSQLQueryRunner):
             column_list=','.join(columns),
             place_holders=','.join([':{}'.format(column) for column in columns]))
 
+        conn.execute(create_table)
+
+        for row in rows:
+            conn.execute(insert_template, row)
+
+    def _execute_query(self, conn, query):
+        with closing(conn.cursor()) as cursor:
+            cursor.execute(query)
+
+            columns = self.fetch_columns(
+                [(d[0], None) for d in cursor.description])
+
+            rows = []
+            column_names = [c['name'] for c in columns]
+
+            for row in cursor:
+                for i, col in enumerate(row):
+                    guess = _guess_type(col)
+
+                    if columns[i]['type'] is None:
+                        columns[i]['type'] = guess
+                    elif columns[i]['type'] != guess:
+                        columns[i]['type'] = TYPE_STRING
+
+                rows.append(dict(zip(column_names, row)))
+
+        return {'columns': columns, 'rows': rows}
+
+    def run_query(self, query, user):
+        query = query.strip()
+        path = str(self.configuration.get('path'))
+        delimiter = str(self.configuration.get('delimiter', ','))
+        if delimiter == 'TAB':
+            delimiter = "\t"
+
         with closing(sqlite3.connect(':memory:')) as conn:
-            conn.execute(create_table)
+            self._load_csv_to_table(conn, path, delimiter)
 
-            for row in rows:
-                conn.execute(insert_template, row)
+            results = self._execute_query(conn, query)
 
-            with closing(conn.cursor()) as cursor:
-                cursor.execute(query)
-
-                columns = self.fetch_columns(
-                    [(d[0], None) for d in cursor.description])
-
-                rows = []
-                column_names = [c['name'] for c in columns]
-
-                for row in cursor:
-                    for i, col in enumerate(row):
-                        guess = _guess_type(col)
-
-                        if columns[i]['type'] is None:
-                            columns[i]['type'] = guess
-                        elif columns[i]['type'] != guess:
-                            columns[i]['type'] = TYPE_STRING
-
-                    rows.append(dict(zip(column_names, row)))
-
-            return json.dumps({'columns': columns, 'rows': rows}, cls=JSONEncoder), None
+        return json.dumps(results, cls=JSONEncoder), None
 
 
 register(QueryableCsv)
