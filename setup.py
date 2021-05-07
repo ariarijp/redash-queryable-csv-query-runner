eabae0f1f536a7f483d67fc4ac08592c12dffb0f
ariarijp <takuya.arita@gmail.com>
2021-05-08 01:20:25 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='redash_queryable_csv_query_runner',
-    version='0.1.0',
+    version='0.1.1',
     description='Queryable CSV Query Runner for Redash',
     author='Takuya Arita',
     author_email='takuya.arita@gmail.com',
